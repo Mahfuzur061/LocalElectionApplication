e99dc768843b35162301003b3b21e675cc697fdb
Mahfuzur061 <mahf0017@stud.kea.dk>
2021-12-20 18:39:09 +0100

    Initial commit

---
+++ src/main/java/com/example/localelectionapplication/LocalElectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.localelectionapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalElectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalElectionApplication.class, args);
+    }
+
+}
